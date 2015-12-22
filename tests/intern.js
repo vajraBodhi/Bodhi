c8cf9e02e905b1234635335993ae8571db7fcfe3
vajraBodhi <820940259@qq.com>
2015-12-22 21:46:46 +0800

    add tests folder

---
+++ tests/intern.js
@@ -0,0 +1,135 @@
+// These default settings work OK for most people. The options that *must* be changed below are the
+// packages, suites, excludeInstrumentation, and (if you want functional tests) functionalSuites.
+// dojoConfig needs to be defined here, otherwise it's too late to affect the dojo loader api
+/* global dojoConfig */
+/* jshint -W020 */
+dojoConfig = {
+  parseOnLoad: false,
+  async: true,
+  tlmSiblingOfDojo: false,
+  has: {
+    'extend-esri': 1
+  },
+  requestProvider: 'dojo/request/registry'
+};
+
+define([], function(){
+  return {
+    // The port on which the instrumenting proxy will listen
+    proxyPort: 9000,
+
+    // A fully qualified URL to the Intern proxy
+    proxyUrl: 'http://localhost:9000/',
+
+    // Default desired capabilities for all environments. Individual capabilities
+    // can be overridden by any of thespecified browser environments in the
+    // `environments` array below as well. See
+    // https://code.google.com/p/selenium/wiki/DesiredCapabilities
+    // for standard Selenium capabilities and
+    // https://saucelabs.com/docs/additional-config#desired-capabilities
+    // for Sauce Labs capabilities.
+    // Note that the `build` capability will be filled in with the current commit
+    // ID from the Travis CI environment
+    // automatically
+    capabilities: {
+      'selenium-version': '2.45.0'
+    },
+
+    // Browsers to run integration testing against. Note that version numbers must
+    // be strings if used with Sauce OnDemand. Options that will be permutated are
+    // browserName, version, platform, and platformVersion; any other
+    // capabilities options specified for an environment will be copied as-is
+    environments: [
+      // { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
+      // { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
+      // { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
+      // { browserName: 'firefox', version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
+      // { browserName: 'chrome', version: '34', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] }
+      // { browserName: 'safari', version: '6', platform: 'OS X 10.8' },
+      // { browserName: 'safari', version: '7', platform: 'OS X 10.9' }
+      //{ browserName: 'firefox' },
+      { browserName: 'chrome' }
+    ],
+
+    // Maximum number of simultaneous integration tests that should be executed on
+    // the remote WebDriver service
+    maxConcurrency: 3,
+
+    // Name of the tunnel class to use for WebDriver tests
+    tunnel: 'NullTunnel',
+
+    // The desired AMD loader to use when running unit tests
+    // (client.html/client.js). Omit to use the default Dojo loader
+    loaders: {
+      //relative path from the launcher
+      'host-node': '../../../../client/stemapp/arcgis-js-api/dojo/dojo',
+      //relative path to the path of intern installed
+      'host-browser': '../../../client/stemapp/arcgis-js-api/dojo/dojo.js'
+      //default
+      //'host-node': 'dojo/dojo', //a Node.js module ID
+      //'host-browser': 'node_modules/dojo/dojo.js'
+    },
+
+    // Configuration options for the module loader; any AMD configuration options
+    // supported by the specified AMD loader
+    // can be used here
+    loaderOptions: {
+      // Packages that should be registered with the loader in each testing environment
+      packages: [{
+        name: 'dojo',
+        location: 'client/stemapp/arcgis-js-api/dojo'
+      }, {
+        name: 'dijit',
+        location: 'client/stemapp/arcgis-js-api/dijit'
+      }, {
+        name: 'dojox',
+        location: 'client/stemapp/arcgis-js-api/dojox'
+      }, {
+        name: 'put-selector',
+        location: 'client/stemapp/arcgis-js-api/put-selector'
+      }, {
+        name: 'xstyle',
+        location: 'client/stemapp/arcgis-js-api/xstyle'
+      }, {
+        name: 'dgrid',
+        location: 'client/stemapp/arcgis-js-api/dgrid'
+      }, {
+        name: 'esri',
+        location: 'client/stemapp/arcgis-js-api/esri'
+      }, {
+        name: 'jimu',
+        location: 'client/stemapp/jimu.js'
+      }, {
+        name: 'themes',
+        location: 'client/stemapp/themes'
+      }, {
+        name: 'libs',
+        location: 'client/stemapp/libs'
+      }, {
+        name: 'dynamic-modules',
+        location: 'client/stemapp/dynamic-modules'
+      }, {
+        name: 'builder',
+        location: 'client/builder'
+      }, {
+        name: 'widgets',
+        location: 'client/stemapp/widgets'
+      }]
+    },
+
+    reporters: ['html'],
+
+    // Non-functional test suite(s) to run in each browser
+    suites: [
+      // 'tests/unit/client/jimu/jimuTest',
+      // 'tests/unit/client/widgets/widgetsTest'
+    ],
+
+    // Functional test suite(s) to run in each browser once non-functional tests are completed
+    functionalSuites: [ /* 'myPackage/tests/functional' */ ],
+
+    // A regular expression matching URLs to files that should not be included in
+    // code coverage analysis
+    excludeInstrumentation: /(?:tests|node_modules|arcgis-js-api)/
+  };
+});
